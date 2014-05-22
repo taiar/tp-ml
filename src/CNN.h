9db5c39a0b0373c2d52af6efd93be7e2cfad13f3
André Taiar <andre.taiar@taghouse.com.br>
2014-05-22 13:06:40 -0300

    Apenas estilização.

--- src/CNN.h
+++ src/CNN.h
@@ -17,8 +17,8 @@ const UINT g_cCountTestingSample = 10000;
 
 #define GAUSSIAN_FIELD_SIZE (21)
 
-#define RANDOM_PLUS_MINUS_ONE	( (double)(2.0 * rand())/RAND_MAX - 1.0 )	//in [-1, 1] range
-#define RANDOM_ZERO_ONE			( (double) rand() / (RAND_MAX + 1) )		//in [0, 1) range
+#define RANDOM_PLUS_MINUS_ONE ( (double)(2.0 * rand())/RAND_MAX - 1.0 ) //in [-1, 1] range
+#define RANDOM_ZERO_ONE     ( (double) rand() / (RAND_MAX + 1) )    //in [0, 1) range
 
 const double dMaxScaling = 15.0; //like 20 for 20%
 const double dMaxRotation = 15.0;  // like 20.0 for 20 degrees
@@ -30,127 +30,107 @@ const double PI = 2.0 * acos(0.0);
 
 
 #include <math.h>
-
-//#include "global.h"
 #include <cstdlib>
 #include <cstdio>
 
 typedef unsigned char uchar;
 
-#define INPUT_LAYER		0
-#define CONVOLUTIONAL	1
+#define INPUT_LAYER   0
+#define CONVOLUTIONAL 1
 #define FULLY_CONNECTED 2
 
-#define SIGMOID(x) (1.7159*tanh(0.66666667*x)) 
+#define SIGMOID(x) (1.7159*tanh(0.66666667*x))
 #define DSIGMOID(S) (0.66666667/1.7159*(1.7159+(S))*(1.7159-(S)))// derivative of the sigmoid as a function of the sigmoid's output
 
 class Layer;
 
-class FeatureMap
-{
+class FeatureMap {
 public:
-	
-	double bias, dErr_wrtb, diagHessianBias;
-	double *value, *dError;
-	double **kernel, **diagHessian, **dErr_wrtw;
-	int m_nFeatureMapPrev;
+  double bias, dErr_wrtb, diagHessianBias;
+  double *value, *dError;
+  double **kernel, **diagHessian, **dErr_wrtw;
+  int m_nFeatureMapPrev;
 
-	Layer *pLayer;
+  Layer *pLayer;
 
-	void Construct( );
-	void Delete();
+  void Construct( );
+  void Delete();
+  void Clear();
+  void ClearDError();
+  void ClearDiagHessian();
+  void ClearDErrWRTW();
 
-	void Clear();	
-	void ClearDError();
-	void ClearDiagHessian();	
-	void ClearDErrWRTW();
-		
-
-	double Convolute(double *input, int size, int r0, int c0, double *weight, int kernel_size);
-	void Calculate(double *valueFeatureMapPrev, int idxFeatureMapPrev );
-	void BackPropagate(double *valueFeatureMapPrev, int idxFeatureMapPrev, double *dErrorFeatureMapPrev, int dOrder );
-	//void BackPropagate2(double *valueFeatureMapPrev, int idxFeatureMapPrev, double *dErrorFeatureMapPrev );
+  double Convolute(double *input, int size, int r0, int c0, double *weight, int kernel_size);
+  void Calculate(double *valueFeatureMapPrev, int idxFeatureMapPrev );
+  void BackPropagate(double *valueFeatureMapPrev, int idxFeatureMapPrev, double *dErrorFeatureMapPrev, int dOrder );
 };
 
-class Layer
-{
+class Layer {
 public:
-	int m_type;
+  int m_type;
+  int m_SamplingFactor;
 
-	int m_SamplingFactor;
+  Layer *pLayerPrev;
 
-	Layer *pLayerPrev;
+  int m_nFeatureMap;
+  int m_FeatureSize;
+  int m_KernelSize;
 
-	int m_nFeatureMap;
-	int m_FeatureSize;
+  FeatureMap* m_FeatureMap;
 
-	int m_KernelSize;
+  void ClearAll() {
+    for(int i=0; i<m_nFeatureMap; i++) {
+      m_FeatureMap[i].Clear();
+      m_FeatureMap[i].ClearDError();
+      m_FeatureMap[i].ClearDErrWRTW();
+      m_FeatureMap[i].ClearDiagHessian();
+    }
+  }
 
-	FeatureMap* m_FeatureMap;
+  void print(char *fileName, double *array, int size) {
+    FILE *f = fopen(fileName, "w");
+    for(int i=0; i<size; i++)
+      fprintf(f, "%lg\n", array[i]);
+  }
 
-	void ClearAll()
-	{
-		for(int i=0; i<m_nFeatureMap; i++)
-		{
-			m_FeatureMap[i].Clear();
-			m_FeatureMap[i].ClearDError();
-			m_FeatureMap[i].ClearDErrWRTW();
-			m_FeatureMap[i].ClearDiagHessian();
-		}
-	}
+  void Calculate();
+  void BackPropagate(int dOrder, double etaLearningRate);
 
-	void print(char *fileName, double *array, int size)
-	{
-		FILE *f = fopen(fileName, "w");
-		for(int i=0; i<size; i++)
-			fprintf(f, "%lg\n", array[i]);
-	}
-
-	void Calculate();
-	void BackPropagate(int dOrder, double etaLearningRate);
-	//void BackPropagate2();
-
-	void Construct(int type, int nFeatureMap, int FeatureSize, int KernelSize, int SamplingFactor);
-	void Delete();
+  void Construct(int type, int nFeatureMap, int FeatureSize, int KernelSize, int SamplingFactor);
+  void Delete();
 };
 
-
-
-
-
-
-class CCNN
-{
+class CCNN {
 public:
-	CCNN(void);
-	CCNN(int numConvolutionalLayers, int numHiddenLayers,
-		int * convFeatureMaps, int * convKernelSizes,
-		int * convStepSize, int * hiddenLayerUnits, size_t classCnt,
-		size_t sqrtInput);
-	~CCNN(void);
+  CCNN(void);
+  CCNN(int numConvolutionalLayers, int numHiddenLayers,
+    int * convFeatureMaps, int * convKernelSizes,
+    int * convStepSize, int * hiddenLayerUnits, size_t classCnt,
+    size_t sqrtInput);
+  ~CCNN(void);
 
-	Layer *m_Layer;
-	int m_nLayer;
+  Layer *m_Layer;
+  int m_nLayer;
 
-	void ConstructNN();
-	void ConstructNN(
-		int numConvolutionalLayers,     // The number of convolutional layers (placed immediatly after the input layer)
-		int numHiddenLayers,            // The number of hidden, MLP layers at the end
-		int * convFeatureMaps,          // The number of feature maps in each convolutional layer
-		int * convKernelSizes,          // The size of the side of the kernel square
-		int * convStepSize,             // Inverse of the pool size ratio; A step size of 3 means that 1/3rd of the neurons in the previous layer get used
-		int * hiddenLayerUnits,         // The number of units in each hidden layer
-		size_t classCnt,                 // The number of classes in the classification
-		size_t sqrtInput                // The square root of the input (assumes input is a square)
-	);
-	void DeleteNN();
+  void ConstructNN();
+  void ConstructNN(
+    int numConvolutionalLayers,     // The number of convolutional layers (placed immediatly after the input layer)
+    int numHiddenLayers,            // The number of hidden, MLP layers at the end
+    int * convFeatureMaps,          // The number of feature maps in each convolutional layer
+    int * convKernelSizes,          // The size of the side of the kernel square
+    int * convStepSize,             // Inverse of the pool size ratio; A step size of 3 means that 1/3rd of the neurons in the previous layer get used
+    int * hiddenLayerUnits,         // The number of units in each hidden layer
+    size_t classCnt,                 // The number of classes in the classification
+    size_t sqrtInput                // The square root of the input (assumes input is a square)
+  );
+  void DeleteNN();
 
-	void LoadWeights(char *FileName);
-	void LoadWeightsRandom();
-	void SaveWeights(char *FileName);
-	int Calculate(double *input, double *output);
-	void BackPropagate(double *desiredOutput, double eta);
-	void CalculateHessian( );
+  void LoadWeights(char *FileName);
+  void LoadWeightsRandom();
+  void SaveWeights(char *FileName);
+  int Calculate(double *input, double *output);
+  void BackPropagate(double *desiredOutput, double eta);
+  void CalculateHessian( );
 };
 
 #endif
