aa3a1b4d5255b2c51426f851c9560ac0ecf780b0
André Taiar <taiar@hotmail.com>
2014-06-05 10:12:36 -0300

    Execução.

--- src/3_4_cnn_vs_mlp.sh
+++ src/3_4_cnn_vs_mlp.sh
@@ -1,70 +1,62 @@
-echo 'EXECUCAO CNN 1'
-./cnn 2 13 5 5 5 2 2 1 100 30 >> ex
+echo 'EXECUCAO CNN 1'           >> saida
+./cnn 2 13 5 5 5 2 2 1 100 30   >> saida
 
 sleep 2
-echo 'EXECUCAO CNN 2'
-./cnn 2 13 5 5 5 2 2 1 100 30 >> ex
+echo 'EXECUCAO CNN 2'           >> saida
+./cnn 2 13 5 5 5 2 2 1 100 30   >> saida
 
 sleep 2
-echo 'EXECUCAO CNN 3'
-./cnn 2 13 5 5 5 2 2 1 100 30 >> ex
+echo 'EXECUCAO CNN 3'           >> saida
+./cnn 2 13 5 5 5 2 2 1 100 30   >> saida
 
 sleep 2
-echo 'EXECUCAO CNN 4'
-./cnn 2 13 5 5 5 2 2 1 100 30 >> ex
+echo 'EXECUCAO CNN 4'           >> saida
+./cnn 2 13 5 5 5 2 2 1 100 30   >> saida
 
 sleep 2
-echo 'EXECUCAO CNN 5'
-./cnn 2 13 5 5 5 2 2 1 100 30 >> ex
+echo 'EXECUCAO CNN 5'           >> saida
+./cnn 2 13 5 5 5 2 2 1 100 30   >> saida
 
 sleep 2
-echo 'EXECUCAO CNN 6'
-./cnn 2 13 5 5 5 2 2 1 100 30 >> ex
+echo 'EXECUCAO CNN 6'           >> saida
+./cnn 2 13 5 5 5 2 2 1 100 30   >> saida
 
 sleep 2
-echo 'EXECUCAO CNN 7'
-./cnn 2 13 5 5 5 2 2 1 100 30 >> ex
+echo 'EXECUCAO CNN 7'           >> saida
+./cnn 2 13 5 5 5 2 2 1 100 30   >> saida
 
 sleep 2
-echo 'EXECUCAO CNN 8'
-./cnn 2 13 5 5 5 2 2 1 100 30 >> ex
+echo 'EXECUCAO CNN 8'           >> saida
+./cnn 2 13 5 5 5 2 2 1 100 30   >> saida
 
 sleep 2
-echo 'EXECUCAO MLP 1' >> ex
-
-./cnn 0 1 100 30
-
-sleep 2
-echo 'EXECUCAO MLP 2' >> ex
-
-./cnn 0 1 100 30
+echo 'EXECUCAO MLP 1'   >> saida
+./cnn 0 1 100 30        >> saida
 
 sleep 2
-echo 'EXECUCAO MLP 3' >> ex
-
-./cnn 0 1 100 30
-
-sleep 2
-echo 'EXECUCAO MLP 4' >> ex
-
-./cnn 0 1 100 30
+echo 'EXECUCAO MLP 2'   >> saida
+./cnn 0 1 100 30        >> saida
 
 sleep 2
-echo 'EXECUCAO MLP 5' >> ex
-
-./cnn 0 1 100 30
-
-sleep 2
-echo 'EXECUCAO MLP 6' >> ex
-
-./cnn 0 1 100 30
+echo 'EXECUCAO MLP 3'   >> saida
+./cnn 0 1 100 30        >> saida
 
 sleep 2
-echo 'EXECUCAO MLP 7' >> ex
-
-./cnn 0 1 100 30
+echo 'EXECUCAO MLP 4'   >> saida
+./cnn 0 1 100 30        >> saida
 
 sleep 2
-echo 'EXECUCAO MLP 8' >> ex
+echo 'EXECUCAO MLP 5'   >> saida
+./cnn 0 1 100 30        >> saida
 
-./cnn 0 1 100 30
+sleep 2
+echo 'EXECUCAO MLP 6'   >> saida
+./cnn 0 1 100 30        >> saida
+
+sleep 2
+echo 'EXECUCAO MLP 7'   >> saida
+./cnn 0 1 100 30        >> saida
+
+sleep 2
+echo 'EXECUCAO MLP 8'   >> saida
+./cnn 0 1 100 30        >> saida
