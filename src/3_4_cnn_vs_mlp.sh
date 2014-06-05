20f649598b3dcdf4fcf97a5a972e9a7c3e69e8c7
André Taiar <taiar@hotmail.com>
2014-06-05 02:29:49 -0300

    Espaçando execuções.

--- src/3_4_cnn_vs_mlp.sh
+++ src/3_4_cnn_vs_mlp.sh
@@ -1,50 +1,66 @@
 make clean
 make main
 
+sleep 2
 echo 'EXECUCAO CNN 1'
 ./cnn 2 13 5 5 5 2 2 1 100 30
 
+sleep 2
 echo 'EXECUCAO CNN 2'
 ./cnn 2 13 5 5 5 2 2 1 100 30
 
+sleep 2
 echo 'EXECUCAO CNN 3'
 ./cnn 2 13 5 5 5 2 2 1 100 30
 
+sleep 2
 echo 'EXECUCAO CNN 4'
 ./cnn 2 13 5 5 5 2 2 1 100 30
 
+sleep 2
 echo 'EXECUCAO CNN 5'
 ./cnn 2 13 5 5 5 2 2 1 100 30
 
+sleep 2
 echo 'EXECUCAO CNN 6'
 ./cnn 2 13 5 5 5 2 2 1 100 30
 
+sleep 2
 echo 'EXECUCAO CNN 7'
 ./cnn 2 13 5 5 5 2 2 1 100 30
 
+sleep 2
 echo 'EXECUCAO CNN 8'
 ./cnn 2 13 5 5 5 2 2 1 100 30
 
+sleep 2
 echo 'EXECUCAO MLP 1'
 ./cnn 0 1 100 30
 
+sleep 2
 echo 'EXECUCAO MLP 2'
 ./cnn 0 1 100 30
 
+sleep 2
 echo 'EXECUCAO MLP 3'
 ./cnn 0 1 100 30
 
+sleep 2
 echo 'EXECUCAO MLP 4'
 ./cnn 0 1 100 30
 
+sleep 2
 echo 'EXECUCAO MLP 5'
 ./cnn 0 1 100 30
 
+sleep 2
 echo 'EXECUCAO MLP 6'
 ./cnn 0 1 100 30
 
+sleep 2
 echo 'EXECUCAO MLP 7'
 ./cnn 0 1 100 30
 
+sleep 2
 echo 'EXECUCAO MLP 8'
 ./cnn 0 1 100 30
