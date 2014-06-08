fd768edd1c8a288fa874562b923de2a9aa8b8ef2
André Taiar <taiar@hotmail.com>
2014-06-08 12:49:41 -0300

    Melhorando a nossa analise.

--- saidas/3_4_analise.php
+++ saidas/3_4_analise.php
@@ -8,14 +8,17 @@
     private $extra;
 
     private $file;
-    private $file_handle;
     private $file_source;
     private $file_size;
     private $file_line;
 
+    public $standard_deviation_epoch;
+    public $mean_epoch;
+    public $standard_deviation_class;
+
     private $num_runs = 0;
 
-    private $runs;
+    public $runs;
 
     public function __construct($file = '', $classes = 10, $epochs = 30, $extra = 8) {
       $this->classes = $classes;
@@ -49,6 +52,33 @@
         }
         $this->runs[] = $run;
       }
+      $this->file_source = array();
+    }
+
+    public function proccess() {
+      $this->standard_deviation_epoch = array();
+      $arr = array();
+      $mean = 0;
+      for ($i=0; $i < $this->epochs; $i++) {
+        $mean = 0;
+        $arr = array();
+        for ($j=0; $j < $this->num_runs; $j++) {
+          $arr[] = $this->runs[$j]->get_epoch($i)['por'];
+          $mean += $this->runs[$j]->get_epoch($i)['por'];
+        }
+        $mean = $mean / $this->num_runs;
+        $this->mean_epoch[$i] = $mean;
+        $this->standard_deviation_epoch[$i] = $this->deviation($arr, $mean);
+      }
+    }
+
+    private function deviation($arr, $mean) {
+      $n = count($arr);
+      $variance = 0;
+      for ($i=0; $i < $n; $i++)
+        $variance += ($arr[$i] - $mean)^2;
+      $variance = $variance / $this->num_runs;
+      return sqrt($variance);
     }
 
     private function line_read() {
@@ -95,7 +125,6 @@
 
     public function read_class($s) {
       $base = strpos($s, "/");
-      echo substr($s, strpos($s, "(") + 1, 6) . PHP_EOL;
       $this->class[] = array(
         'num' => substr($s, 3, $base - 3),
         'tot' => substr($s, $base + 1, strpos(substr($s, $base), "(") - 1),
@@ -103,7 +132,64 @@
       );
     }
 
+    public function get_epoch($i) {
+      return $this->epoch[$i];
+    }
+
+    public function get_class($i) {
+      return $this->class[$i];
+    }
   }
 
-  $a = new run_analys('3_4_saida');
+  require_once ('jpgraph/src/jpgraph.php');
+  require_once ('jpgraph/src/jpgraph_log.php');
+  require_once ('jpgraph/src/jpgraph_bar.php');
+
+  $a = new run_analys('3_4_saida_cnn');
   $a->read();
+  $a->proccess();
+
+  $b = new run_analys('3_4_saida_mlp');
+  $b->read();
+  $b->proccess();
+
+  $graph_a = new Graph(800, 500, 'auto');
+  $graph_a->SetScale("intint");
+  $graph_a->SetY2Scale("log");
+
+  $theme_class = new UniversalTheme;
+  $graph_a->SetTheme($theme_class);
+
+  $graph_a->yaxis->SetTickPositions(array(50,60,70,80,90,100),
+    array(55,65,75,85,95));
+
+  $graph_a->SetBox(false);
+
+  $graph_a->ygrid->SetFill(false);
+  $graph_a->xaxis->SetTickLabels(array('1','2','3','4','5','6','7','8','9','10','11','12','13','14',
+    '15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30'));
+  $graph_a->yaxis->HideLine(false);
+  $graph_a->yaxis->HideTicks(false,false);
+
+  // Create the bar plots
+  $b1plot = new BarPlot($a->mean_epoch);
+  $b2plot = new BarPlot($b->mean_epoch);
+
+  // Create the grouped bar plot
+  $gbplot = new GroupBarPlot(array($b1plot,$b2plot));
+  $graph_a->Add($gbplot);
+
+  $b1plot->SetColor("white");
+  $b1plot->SetFillColor("#cc1111");
+
+  $b2plot->SetColor("white");
+  $b2plot->SetFillColor("#11cccc");
+
+  $graph_a->title->Set("CNNs");
+
+  $graph_a->Stroke(dirname(__FILE__) . '/cnn.png');
+
+  // echo print_r($a->mean_epoch);
+  // echo print_r($a->standard_deviation_epoch);
+  // echo print_r($b->mean_epoch);
+  // echo print_r($b->standard_deviation_epoch);
